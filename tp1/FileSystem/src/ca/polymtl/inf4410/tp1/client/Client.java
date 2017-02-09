d5ec3b934e4f902ce4808c93a9a36f14761ec91c
Samuel <samuel_lavoie@hotmail.com>
2017-02-08 20:12:34 -0500

    client

--- tp1/FileSystem/src/ca/polymtl/inf4410/tp1/client/Client.java
+++ tp1/FileSystem/src/ca/polymtl/inf4410/tp1/client/Client.java
@@ -1,67 +1,269 @@
 package ca.polymtl.inf4410.tp1.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.Exception;
+import java.lang.StringBuilder;
+import java.lang.System;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.AccessException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import ca.polymtl.inf4410.tp1.shared.ServerInterface;
 
 public class Client {
+
+	private ServerInterface distantServerStub = null;
+	private Map<String, ServerOperation> fnList;
+
 	public static void main(String[] args) {
-		String distantHostname = null;
-		int eOctets = 0;
+		// ***** Entrez ici l'adresse IP du serveur *****
+		String distantHostname = "132.42.42.42";
+		String action = null;
+		String argument = null;
 
 		if (args.length > 0) {
-			distantHostname = args[0];
-			eOctets = Integer.parseInt(args[1]);
+			action = args[0];
+			if (args.length > 1) {
+				argument = args[1];
+			}
 		}
 
-		if (eOctets < 1 || eOctets > 7) {
-			throw new IllegalArgumentException("Le nombre d'octets doit etre entre 10^1 et 10^7");
+		try {
+			Client client = new Client(distantHostname);
+			client.run(action, argument);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		Double nOctets = Math.pow(10, eOctets);
-		Byte[] payload = new Byte[nOctets.intValue()];
 
-		Client client = new Client(distantHostname);
-		client.run(payload);
 	}
 
-	FakeServer localServer = null; // Pour tester la latence d'un appel de
-									// fonction normal.
-	private ServerInterface localServerStub = null;
-	private ServerInterface distantServerStub = null;
+	/**
+	 * Demande au client de generer un UUID si le fichier .client_id n'existe pas.
+	 * Ecrit le UUI dans le fichier .client_id.
+	 * @return Le UUID cree par le client.
+	 */
+	private UUID generateClientId() {
+		try {
+			UUID id = distantServerStub.generateclientid();
+			Files.write(Paths.get(".client_id"), id.toString().getBytes());
+			return id;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Demande de creer un fichier sur le client.
+	 * @param nom - Le nom du fichier.
+	 */
+	private void create(String name) {
+		try {
+			if(distantServerStub.create(name)) {
+				System.out.println(name.concat(" ajouté."));
+			} else {
+				System.out.println(name.concat(" existe déjà."));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Ecrit la liste des fichiers presents sur le fichier.
+	 */
+	private void list() {
+		try {
+			Map<String, UUID> files = distantServerStub.list();
+			for (Map.Entry<String, UUID> file : files.entrySet()) {
+				String owner = file.getValue() == null ? "non verouillé" : file.getValue().toString() ;
+				System.out.println(file.getKey().concat("\t").concat(owner));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Synchronise les fichiers locaux avec les fichiers sur le serveur.
+	 * Les fichiers locaux, si existant, sont ecraser par les fichier venant du serveur.
+	 * Un fichier est créée s'il n'existe pas déjà localement.
+	 */
+	private void syncLocalDir() {
+		try {
+			Map<String, byte[]> files = distantServerStub.syncLocalDir();
+			for (Map.Entry<String, byte[]> file : files.entrySet()) {
+				Files.write(Paths.get(file.getKey()), file.getValue());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Chercher la dernière version d'un fichier sur le serveur.
+	 * @param name - Le nom du fichier à aller chercher.
+	 */
+	private void get(String name) {
+		try {
+			byte[] checksum = getFileChecksum(name);
+			byte[] data = distantServerStub.get(name, checksum);
+			// Le serveur retourne null si la version local est la même que la version serveur.
+			// Écrire seulement si la version serveur est différente.
+			if (data != null) {
+				Files.write(Paths.get(name), data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Demande au serveur de vérouiller un fichier.
+	 * Si l'opération a été concluante, la copie locale est remplacée par la copie serveur.
+	 * @param name - Le nom du fichier à vérouiller.
+	 */
+	private void lock(String name) {
+		try {
+			byte[] checksum = getFileChecksum(name);
+			UUID clientId = getClientId();
+			// Les données retournées par le serveur sont les données du fichier et l'id du client ayant le locké
+			java.util.Map.Entry<byte[],UUID> data = distantServerStub.lock(name, clientId, checksum);
+			// Si l'id retourné par le serveur n'est pas le même que l'id de l'utilisateur, échec du vérouillage.
+			if (!data.getValue().equals(getClientId())) {
+				System.out.println(name.concat(" est déjà verrouillé par ").concat(data.getValue().toString()));
+				return;
+			}
+			// Si l'id retourné par le serveur est le même que l'id de l'utilisateur et
+			// que le fichier serveur est différent que le fichier local, écrire les données localement.
+			if (data != null) {
+				Files.write(Paths.get(name), data.toString().getBytes());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Pousser un fichier vérouiller par l'utilisateur sur le serveur.
+	 * @param name - Nom du fichier à pousser sur le serveur
+	 */
+	private void push(String name) {
+		try {
+			byte[] data;
+			try {
+				data = Files.readAllBytes(Paths.get(name));
+			} catch (IOException e) {
+				throw e;
+			}
+			UUID clientId = getClientId();
+			boolean success = distantServerStub.push(name, data, clientId);
+			if (success) {
+				System.out.println(name.concat(" a été envoyé au serveur"));
+			} else {
+				System.out.println("opération refusée : vous devez verrouiller d'abord verrouiller le fichier.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Retourne l'id de l'utilisateur s'il existe. Sinon génère l'id de l'utilisateur.
+	 * @return L'id de l'utilsiateur
+	 */
+	private UUID getClientId() {
+		try {
+			byte[] content = Files.readAllBytes(Paths.get(".client_id"));
+			ByteBuffer bb = ByteBuffer.wrap(content);
+			long high = bb.getLong();
+			long low = bb.getLong();
+			return new UUID(high, low);
+		} catch (Exception e) {
+			return generateClientId();
+		}
+	}
+
+	/**
+	 * Génère le checksum d'un fichier
+	 * @param name - Le nom du fichier
+	 * @return Le checksum du fichier
+	 */
+	private byte[] getFileChecksum(String name) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] checksum;
+			try {
+				byte[] data = Files.readAllBytes(Paths.get(name));
+				checksum = md.digest(data);
+			} catch (IOException e) {
+				checksum = "-1".getBytes();
+			}
+			return checksum;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	interface ServerOperation {
+		void operation(String arg);
+	}
+
 
 	public Client(String distantServerHostname) {
 		super();
 
+		fnList = new HashMap<String, ServerOperation>();
+			ServerOperation create = (arg) -> create(arg);
+			ServerOperation list = (arg) -> list();
+			ServerOperation syncLocalDir = (arg) -> syncLocalDir();
+			ServerOperation get = (arg) -> get(arg);
+			ServerOperation lock = (arg) -> lock(arg);
+			ServerOperation push = (arg) -> push(arg);
+
+			fnList.put("create", create);
+			fnList.put("list", list);
+			fnList.put("syncLocalDir", syncLocalDir);
+			fnList.put("get", get);
+			fnList.put("lock", lock);
+			fnList.put("push", push);
+
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
 		}
-
-		localServer = new FakeServer();
-		localServerStub = loadServerStub("127.0.0.1");
 
 		if (distantServerHostname != null) {
 			distantServerStub = loadServerStub(distantServerHostname);
 		}
 	}
 
-	private void run(Byte[] payload) {
-		appelNormal(payload);
+	private void run(String action, String argument) throws RemoteException {
+		fnList.get(action).operation(argument);
 
-		if (localServerStub != null) {
-			appelRMILocal(payload);
-		}
 
-		if (distantServerStub != null) {
-			appelRMIDistant(payload);
-		}
 	}
 
-	private ServerInterface loadServerStub(String hostname) {
+	private ServerInterface loadServerStub(String hostname){
 		ServerInterface stub = null;
 
 		try {
@@ -79,41 +281,4 @@ public class Client {
 		return stub;
 	}
 
-	private void appelNormal(Byte[] payload) {
-		long start = System.nanoTime();
-		int result = localServer.execute(payload, payload);
-		long end = System.nanoTime();
-
-		System.out.println("Temps écoulé appel normal: " + (end - start)
-				+ " ns");
-		System.out.println("Résultat appel normal: " + result);
-	}
-
-	private void appelRMILocal(Byte[] payload) {
-		try {
-			long start = System.nanoTime();
-			int result = localServerStub.execute(payload, payload);
-			long end = System.nanoTime();
-
-			System.out.println("Temps écoulé appel RMI local: " + (end - start)
-					+ " ns");
-			System.out.println("Résultat appel RMI local: " + result);
-		} catch (RemoteException e) {
-			System.out.println("Erreur: " + e.getMessage());
-		}
-	}
-
-	private void appelRMIDistant(Byte[] payload) {
-		try {
-			long start = System.nanoTime();
-			int result = distantServerStub.execute(payload, payload);
-			long end = System.nanoTime();
-
-			System.out.println("Temps écoulé appel RMI distant: "
-					+ (end - start) + " ns");
-			System.out.println("Résultat appel RMI distant: " + result);
-		} catch (RemoteException e) {
-			System.out.println("Erreur: " + e.getMessage());
-		}
-	}
 }
